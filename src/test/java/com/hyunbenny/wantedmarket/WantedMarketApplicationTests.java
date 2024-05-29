eb4c771589ca0af4835043c44535e05ea2bfbeb2
hbcho90 <hbcho90@inzent.com>
2024-05-29 17:26:04 +0900

    프로젝트 생성

---
+++ src/test/java/com/hyunbenny/wantedmarket/WantedMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunbenny.wantedmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
